098fb8669091ae1e6556013bb4bf01d5847b78e1
Daniel Gordon <d_gordon1@icloud.com>
2019-11-21 09:25:57 +1100

    finished pg_search_scopes.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,19 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  include PgSearch::Model
+
+  # pg_search_scope :title_and_syllabus,
+  #   against: [:title, :syllabus],
+  #   using: {
+  #     tsearch: {prefix: true}
+  #   }
+
+  pg_search_scope :movie_and_director,
+    against: [:title, :syllabus],
+    associated_against: {
+      director: [:first_name, :last_name]
+    },
+    using: {
+      tsearch: {prefix: true}
+    }
 end
